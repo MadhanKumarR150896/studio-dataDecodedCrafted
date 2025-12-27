b4657954deb5b9e9d056c00a50b19e0fea15e98b
MadhanKumarR150896 <madhankumar150896@outlook.com>
2025-12-27 15:22:00 +0530

    Final before deploying

--- sanity.cli.js
+++ sanity.cli.js
@@ -1,14 +1,12 @@
 /* eslint-disable no-undef */
 import {defineCliConfig} from 'sanity/cli'
-import * as dotenv from 'dotenv'
-
-dotenv.config()
 
 export default defineCliConfig({
   api: {
     projectId: process.env.SANITY_STUDIO_PROJECT_ID,
     dataset: process.env.SANITY_STUDIO_DATASET,
   },
+  studioHost: process.env.SANITY_STUDIO_HOSTNAME,
   deployment: {
     /**
      * Enable auto-updates for studios.
--- sanity.config.js
+++ sanity.config.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
 import {visionTool} from '@sanity/vision'
@@ -8,8 +9,8 @@ export default defineConfig({
   name: 'default',
   title: 'DataDecodedCrafted',
 
-  projectId: import.meta.env.SANITY_STUDIO_PROJECT_ID,
-  dataset: import.meta.env.SANITY_STUDIO_DATASET,
+  projectId: process.env.SANITY_STUDIO_PROJECT_ID,
+  dataset: process.env.SANITY_STUDIO_DATASET,
 
   plugins: [structureTool(), visionTool(), codeInput()],
 
